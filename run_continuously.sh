51cd8920448108560fd5c7b856f91c005b63ca01
Andrew Ballman <andrew.ballman@checkr.com>
2019-08-26 17:39:54 -0700

    Change to 30m from 1h

--- run_continuously.sh
+++ run_continuously.sh
@@ -4,6 +4,6 @@ while true
 do
   echo "Starting curator"
   /usr/local/bin/curator --config $CONFIG_FILE $ACTION_FILE
-  echo "finished cleaning indexes..sleeping for 1h"
-  sleep 1h
+  echo "finished cleaning indexes..sleeping for 30m"
+  sleep 30m
 done
